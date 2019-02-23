2cd4a0b7090c2e202ce9a9725c741c89dfc6f659
Renan Kaic Lopes <renan-kaic@outlook.com>
2019-02-23 18:23:36 -0300

    Gets the form fields values and create a new table row in table tbody using TemplateString

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
-fields.forEach(function(field, index){
+function addLine(dataUser){
 
-    if ( field.name == "gender" && field.checked ){
+    //Create a tr element to put in a table tbody
+    var tr = document.createElement("tr");
 
-        user[field.name] = field.value;
+    //Uses the template string to create a table row
+    tr.innerHTML = `
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+    `;
 
-    } else {
+    //Appends the new created table row to table's tbody
+    document.querySelector("#table-users tbody").appendChild(tr);
 
-        user[field.name] = field.value;
+}
 
-    }
+
+//Submit button event
+document.getElementById("form-user-create").addEventListener("submit", function(event) {
+    
+    //cancels the default command that the event would have
+    event.preventDefault();
+    
+    //gets all values from fields and put in the user array
+    fields.forEach(function(field, index){
+
+        if ( field.name == "gender" && field.checked ){
+    
+            user[field.name] = field.value;
+    
+        } else {
+    
+            user[field.name] = field.value;
+    
+        }
+    
+    });
+
+    //adds a user line in table
+    addLine(user);
 
 });
-
-console.log(user);
