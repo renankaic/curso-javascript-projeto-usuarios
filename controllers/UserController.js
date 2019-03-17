c22a6520c935bc5232a3dc34491d77abda8c936a
Renan Kaic Lopes <renan-kaic@outlook.com>
2019-03-17 18:25:33 -0300

    Adding event tto delete button

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -257,7 +257,7 @@ class UserController {
             <td>${dataUser.register.toLocaleDateString('pt-br') + " " + dataUser.register.toLocaleTimeString('pt-br')}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
             </td>
         `;
 
@@ -271,6 +271,18 @@ class UserController {
     }//Closing addline()
 
     addEventsTr(tr){
+
+        tr.querySelector(".btn-delete").addEventListener("click", e => {
+
+            if (confirm("Deseja realmente excluir?")){
+
+                tr.remove();
+                
+                this.updateCount();
+
+            }
+
+        });
 
         tr.querySelector(".btn-edit").addEventListener("click", e => {
 
